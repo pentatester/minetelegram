ce56fa8dc21e663e4f6735f91c232ca4698dd2ca
hexatester <revolusi147id@gmail.com>
2020-06-07 16:54:30 +0700

    v0.5.0 initial release

--- README.md
+++ README.md
@@ -8,9 +8,17 @@ Minecraft - Telegram bridge, build on top of mineflayer &amp; telegraf.
 
 ## Usage
 
+### Standalone
+
+```bash
+minetelegram --token BOT_TOKEN --id TELEGRAM_USER_ID --username myusername --server play.myserver.mine --mcversion 1.13.2
+```
+
+If you stuck? do `minetelegram --help`
+
 ```txt
 minetelegram [-h] [-v] -t TOKEN -i ID -u USERNAME [-p PASSWORD] [-s SERVER]
-                [--port PORT] [-mcv MCVERSION]
+                [--port PORT] [-mcv MCVERSION] [-e {on,off}]
 
 
 Minecraft - Telegram bridge
@@ -19,8 +27,8 @@ Optional arguments:
   -h, --help            Show this help message and exit.
   -v, --version         Show program's version number and exit.
   -t TOKEN, --token TOKEN
-                        telegram bot token from @botfather
-  -i ID, --id ID        your telegram user id
+                        telegram bot token, created from @botfather
+  -i ID, --id ID        your telegram user id, get your id from @myidbot
   -u USERNAME, --username USERNAME
                         minecraft username / email
   -p PASSWORD, --password PASSWORD
@@ -30,8 +38,50 @@ Optional arguments:
   --port PORT           minecraft server port, default : 25565
   -mcv MCVERSION, --mcversion MCVERSION
                         minecraft version, eg "1.13.2"
+  -e {on,off}, --echo {on,off}
+                        echo everything to console?, default : on
 ```
+
+### As mineflayer plugin
+
+```js
+const mineflayer = require("mineflayer");
+const bot = mineflayer.createBot({
+  host: "localhost", // optional
+  port: 25565, // optional
+  username: "email@example.com", // email and password are required only for
+  password: "12345678", // online-mode=true servers
+  version: false, // false corresponds to auto version detection (that's the default), put for example "1.8.8" if you need a specific version
+});
+
+const minetelegram = require("minetelegram");
+
+BOT_TOKEN = "your:bot_token"; // telegram bot token, created from @botfather
+TELEGRAM_ID = ""; // your telegram user id, get your id from @myidbot
+ECHO = true; // echo everything to console?, default : true
+
+minetelegram(bot, BOT_TOKEN, TELEGRAM_ID, ECHO);
+```
+
+#### API
+
+##### Telegram
+
+After you register minetelegram using `minetelegram(bot, BOT_TOKEN, TELEGRAM_ID, ECHO)`
+
+You can access [telegraf/Telegraf](https://telegraf.js.org/#/?id=telegraf) from `bot.telegraf`,
+also [telegraf/Telegram](https://telegraf.js.org/#/?id=telegram) from `bot.telegram`.
+
+TL;DR
+[telegraf/Telegraf](https://telegraf.js.org/#/?id=telegraf) mainly used to listen for events from telegram.
+[telegraf/Telegram](https://telegraf.js.org/#/?id=telegram) mainly used to send update / message into telegram.
+
+##### Methods
+
+`bot.addToIgnore(text)` to ignore text / message to be sent when listen mode is enabled
+`bot.sendMessage(message)` send text message to telegram.
 
 ## Roadmap
 
 - Support inventory
+- Notify on pm
--- index.js
+++ index.js
@@ -1,83 +1,144 @@
-if (process.argv.length < 3 || process.argv.length > 6) {
-  console.log(
-    "Usage : node index.js <bot_token> <telegram_id> <mc_username> <mc_password>"
-  );
-  process.exit(1);
-}
+const VERSION = '0.5.0'
+const assert = require('assert')
 
-const { Telegraf } = require("telegraf");
-
-const { MenuTemplate, MenuMiddleware } = require("telegraf-inline-menu");
-
-const { createBot } = require("mineflayer");
-
-const telegram = new Telegraf(process.argv[2]);
-
-const admin = parseInt(process.argv[3], 10);
-
-const username_ = process.argv[4];
-const password_ = process.argv[5];
-
-let bot;
-let listen = false;
-
-telegram.use(async (ctx, next) => {
-  // middleware
-  if (ctx.chat.id === admin) {
-    await next();
+function inject (bot, token, user, echo = true, ignoreMessages = []) {
+  assert.ok(token, new Error('Token not found! pm @botfather to get one'))
+  assert.ok(user, new Error('User id not found! pm @myidbot to get one'))
+  user = parseInt(user)
+  const { Telegraf, Telegram } = require('telegraf')
+  const telegram = new Telegraf(token)
+  const telegramClient = new Telegram(token)
+  let listen = true
+  if (user) {
+    telegram.use(async (ctx, next) => {
+      if (ctx.chat.id === user) {
+        await next()
+      }
+    })
   }
-});
 
-function pm(message) {
-  telegram.sendMessage(admin, message);
-}
+  // Ignored messages
+  let ignores = ['/start', '/listen', '/ignore'].concat(ignoreMessages)
+  const ignore = msg => ignores.includes(msg)
 
-function toggle(mode) {
-  if (mode) {
-    listen = mode;
-  } else {
-    listen = !listen;
-  }
-  if (listen) {
-    ctx.reply("Listen mode : ON");
-  } else {
-    ctx.reply("Listen mode : OFF");
-  }
-}
+  // /start command
+  telegram.start(ctx =>
+    ctx.replyWithHTML(`
+    Hi!
+    Usage :
+    <code>/listen</code> - To toggle listen mode
+    <code>/ignore <text></code> - Prevent message to be sent into bot.chat on listen mode, eg <code>/ignore /start</code>
+    `)
+  )
 
-telegram.command("toggle", (ctx) => toggle());
-
-telegram.command("connect", (ctx) => {
-  if (ctx.message && ctx.message.text.split(" ").length === 3) {
-    let con = ctx.message.text.split(" ");
-    try {
-      let bot = createBot({
-        username: username_,
-        password: password_,
-        host: con[1],
-        port: con[2],
-      });
-      toggle(true);
-      bot.on("message", (message) => {
-        try {
-          if (listen) return pm(message);
-        } catch (error) {
-          return pm(`Error : ${error.message}`);
-        }
-      });
-      telegram.on("message", (ctx) => {
-        try {
-          if (bot !== null && listen) return bot.chat(ctx.message.text);
-        } catch (error) {
-          return pm(`Error : ${error.message}`);
-        }
-      });
-    } catch (error) {
-      ctx.reply(`Error : ${error.message}`);
+  // /ignore command - ignore message
+  telegram.command('ignore', ctx => {
+    const message = ctx.message.text || ''
+    const text = message.split(' ')[-1]
+    if (ignores.includes(text)) {
+      return ctx.reply(`${text} already in ignore list`)
     }
-  } else {
-    ctx.reply("Usage : /connect <host> <port>");
-  }
-});
+    try {
+      ignores.push(text)
+    } catch (error) {
+      return ctx.reply(`Failed to ignore, reason : ${error.message}`)
+    }
+    return ctx.reply(`Successfuly added ${text} into ignore list`)
+  })
 
-telegram.start((ctx) => ctx.reply("Hi."));
+  // /listen command
+  telegram.command('listen', ({ reply }) => {
+    if (listen) {
+      listen = false
+      reply('Listen mode disabled')
+    } else {
+      listen = true
+      reply('Listen mode enabled')
+    }
+  })
+
+  // Telegram -> bot.chat if listen === true
+  telegram.on('text', ctx => {
+    if (
+      listen &&
+      ctx.message &&
+      ctx.message.text &&
+      !ignore(ctx.message.text)
+    ) {
+      return bot.chat(ctx.message.text)
+    }
+    if (echo) console.log(`<- ${ctx.message.text}`)
+  })
+
+  // Minecraft chat -> telegram
+  bot.on('message', message => {
+    let length = message && message.length ? message.length() : 0
+    if (listen && message && length > 0) {
+      telegramClient.sendMessage(user, message.toString())
+    }
+    if (echo) console.log(`-> ${message.toString() || message}`)
+  })
+
+  // Inject into bot object
+  bot.telegraf = telegram
+  bot.telegram = telegramClient
+  bot.sendMessage = text => telegramClient.sendMessage(user, text)
+  bot.addToIgnore = text => ignores.push(text)
+  telegram.launch()
+}
+
+// Check if directly executed?
+if (require.main === module) {
+  const { ArgumentParser } = require('argparse')
+  const parser = new ArgumentParser({
+    version: VERSION,
+    addHelp: true,
+    description: 'Minecraft - Telegram bridge'
+  })
+  parser.addArgument(['-t', '--token'], {
+    help: 'telegram bot token, created from @botfather',
+    required: true
+  })
+  parser.addArgument(['-i', '--id'], {
+    help: 'your telegram user id, get your id from @myidbot',
+    required: true
+  })
+  parser.addArgument(['-u', '--username'], {
+    help: 'minecraft username / email',
+    required: true
+  })
+  parser.addArgument(['-p', '--password'], {
+    help: 'minecraft password, only for online-mode=true servers '
+  })
+  parser.addArgument(['-s', '--server'], {
+    defaultValue: 'localhost',
+    help: 'minecraft server address, default : "localhost"'
+  })
+  parser.addArgument(['--port'], {
+    defaultValue: 25565,
+    help: 'minecraft server port, default : 25565'
+  })
+  parser.addArgument(['-mcv', '--mcversion'], {
+    help: 'minecraft version, eg "1.13.2"'
+  })
+  parser.addArgument(['-e', '--echo'], {
+    defaultValue: 'on',
+    choices: ['on', 'off'],
+    help: 'echo everything to console?, default : on'
+  })
+  const args = parser.parseArgs()
+  const botOptions = {
+    host: args.server || 'localhost',
+    port: args.port || 25565,
+    username: args.username,
+    password: args.password,
+    'online-mode': typeof args.password === 'string',
+    version: args.mcversion || false
+  }
+  const { createBot } = require('mineflayer')
+  const bot = createBot(botOptions)
+  const echo = args.echo && args.echo === 'on'
+  inject(bot, args.token, args.id, echo)
+}
+
+module.exports = inject
