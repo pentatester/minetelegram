917f10b9f2ea5f81440f04d3f9449cdd7bc9ae3d
hexatester <revolusi147id@gmail.com>
2020-06-27 16:00:07 +0700

    add chest related commands

--- index.js
+++ index.js
@@ -56,6 +56,10 @@ class Minetelegram {
       '/consume',
       '/use_item',
       '/toss',
+      '/chest',
+      '/close_chest',
+      '/deposit_chest',
+      '/withdraw_chest',
       '/helps',
       '/quit'
     ]
